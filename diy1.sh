095af8623fd9ea616010f75884a97f5fee2c3ce9
OmyK <31337777+icons88@users.noreply.github.com>
2025-12-24 14:09:14 +0800

    Create diy1.sh

---
+++ diy1.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
